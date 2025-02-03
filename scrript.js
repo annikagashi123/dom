e99a6fe153ba52a53d6e350efb860e9626dcd865
annikagashi123 <annikagashi18@gmail.com>
2025-02-03 10:46:34 -0800

    domi

---
+++ scrript.js
@@ -0,0 +1,14 @@
+//selektoret
+//slecton elementin e pare te tipit p
+// let paragarfi = document.querySelector("p"); //objekt
+// console.log(paragarfi);
+
+//selekton te  gjithe elementet e tipit p
+// let paragarfi = document.querySelectorAll("p"); //varg
+// console.log(paragarfi[0] ,textContent);
+
+let pararafiMeklase = document.querySelector(".par2");
+let paragrafiMeklase2 = document.getElementsByClassName("par1");
+
+console.log(pararafiMeklase);
+pararafiMeklase.textContent = "mirjeta";
